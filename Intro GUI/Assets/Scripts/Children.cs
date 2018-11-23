78f8242229ae5fac65bc57ad6b8cd59f94e94e93
Diendophyx <42424060+Diendophyx@users.noreply.github.com>
2018-11-23 15:27:44 +1100

    All of first day
    Can change skin, hair, clothes, eyes and mouth, now just need to make simpler and make smaller, individual scripts

---
+++ Intro GUI/Assets/Scripts/Children.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Children : MonoBehaviour
+{
+    public GameObject children;
+    bool fuck;
+    // Use this for initialization
+    void Start()
+    {
+        fuck = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void EnabeKids()
+    {
+        if (fuck)
+        {
+            children.SetActive(true);
+            fuck = false;
+        }
+        else
+        {
+            children.SetActive(false);
+            fuck = true;
+        }
+    }
+
+    public void disableothers()
+    {
+
+    }
+}
